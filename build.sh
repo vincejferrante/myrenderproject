0016dcd7a647705835e4df979b926201d4ba3849
vincejferrante <vincejferrante@gmail.com>
2025-07-26 16:47:47 -0700

    Fix: install dependencies in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations and collect static files
 python manage.py migrate
 python manage.py collectstatic --noinput
